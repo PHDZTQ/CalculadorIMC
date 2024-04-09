1c42165da47cd98cdeee1d73c5ae84dcdec13043
Paola Hernandez <paolaht12@gmail.com>
2024-04-08 20:20:30 -0600

    Se crearon 11 archivos entre ellos archivos kotlin y archivos xml ya que se creo el recyclerView de las tareas, en donde de debio crear 1 archivo kotlin para el adaptador y otro para el viewHolder asi como un xml para el item, ademas se implemento el boton flotante para asi abrir nuestro dialogo en donde ingresaremos una tarea nueva

--- app/src/main/java/com/pht/calculadorimc/MenuActivity.kt
+++ app/src/main/java/com/pht/calculadorimc/MenuActivity.kt
@@ -3,9 +3,11 @@ package com.pht.calculadorimc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
 import com.pht.calculadorimc.firstapp.FirstAppActivity
 import com.pht.calculadorimc.imccalculator.ImcCalculatorActivity
+import com.pht.calculadorimc.todoapp.TodoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +15,10 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         val btnSaludoApp = findViewById<AppCompatButton>(R.id.btnSaludoApp)
         val btnIMCApp = findViewById<AppCompatButton>(R.id.btnIMCApp)
+        val btnTodoApp = findViewById<AppCompatButton>(R.id.btnTodoApp)
         btnSaludoApp.setOnClickListener { navigateToSaludoApp() }
         btnIMCApp.setOnClickListener { navigateToIMCApp() }
+        btnTodoApp.setOnClickListener { navigateToTodoApp() }
     }
 
     private fun navigateToSaludoApp() {
@@ -24,6 +28,11 @@ class MenuActivity : AppCompatActivity() {
 
     private fun navigateToIMCApp() {
         val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToTodoApp() {
+        val intent = Intent(this, TodoActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/pht/calculadorimc/todoapp/TodoActivity.kt
@@ -0,0 +1,70 @@
+package com.pht.calculadorimc.todoapp
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.pht.calculadorimc.R
+import com.pht.calculadorimc.todoapp.TaskCategory.*
+
+class TodoActivity : AppCompatActivity() {
+
+    private val categories = listOf(
+        Personal,
+        Business,
+        Other
+    )
+
+    private val tasks = mutableListOf(
+        Task("PruebaBusiness", Business),
+        Task("PruebaPersonal", Personal),
+        Task("PruebaOther", Other)
+    )
+
+    private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
+
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var taskAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+
+        initComponent()
+        initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener { showDialog() }
+    }
+
+    private fun showDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
+    }
+
+    private fun initComponent() {
+        rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
+    }
+    private fun initUI() {
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
+
+        taskAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = taskAdapter
+    }
+
+}
