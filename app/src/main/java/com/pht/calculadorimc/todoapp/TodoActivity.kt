4d59371fe22bef503a110091e61710d1ea4fc942
Paola Hernandez <paolaht12@gmail.com>
2024-04-10 13:59:35 -0600

    Aplicacion finalizada en donde podemos agregar nuevas tareas y estas se mostraran en nuestras listas, ademas de que podemos marcarlos y desmarcarlos, y se pueden filtrar de acuerdo a la categoria

--- app/src/main/java/com/pht/calculadorimc/todoapp/TodoActivity.kt
+++ app/src/main/java/com/pht/calculadorimc/todoapp/TodoActivity.kt
@@ -2,6 +2,10 @@ package com.pht.calculadorimc.todoapp
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -46,9 +50,31 @@ class TodoActivity : AppCompatActivity() {
         fabAddTask.setOnClickListener { showDialog() }
     }
 
-    private fun showDialog(){
+    private fun showDialog() {
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_task)
+
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnAddTask.setOnClickListener {
+            val currentTask = etTask.text.toString()
+            if (currentTask.isNotEmpty()) {
+                val selectedId = rgCategories.checkedRadioButtonId
+                val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId)
+                val currentCategory: TaskCategory = when (selectedRadioButton.text) {
+                    getString(R.string.todo_dialog_business) -> Business
+                    getString(R.string.todo_dialog_personal) -> Personal
+                    else -> Other
+                }
+
+                tasks.add(Task(currentTask, currentCategory))
+                updateTasks()
+                dialog.hide()
+            }
+        }
+
         dialog.show()
     }
 
@@ -57,14 +83,36 @@ class TodoActivity : AppCompatActivity() {
         rvTasks = findViewById(R.id.rvTasks)
         fabAddTask = findViewById(R.id.fabAddTask)
     }
+
     private fun initUI() {
-        categoriesAdapter = CategoriesAdapter(categories)
-        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        categoriesAdapter =
+            CategoriesAdapter(categories) { position -> updateCategories(position) } // <- Funcion lamda
+        rvCategories.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
 
-        taskAdapter = TasksAdapter(tasks)
+        taskAdapter = TasksAdapter(tasks) { onItemSelected(it) } // <- Funcion lamda
         rvTasks.layoutManager = LinearLayoutManager(this)
         rvTasks.adapter = taskAdapter
+    }
+
+    private fun onItemSelected(position: Int) {
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTasks()
+    }
+
+    private fun updateCategories(position: Int) {
+        categories[position].isSelected = !categories[position].isSelected
+        categoriesAdapter.notifyItemChanged(position)
+        updateTasks()
+    }
+
+    private fun updateTasks() {
+        val selectedCategories: List<TaskCategory> =
+            categories.filter { it.isSelected } //Filtrara las task por la categoria seleccionada
+        val newTasks = tasks.filter { selectedCategories.contains(it.category) }
+        taskAdapter.tasks = newTasks
+        taskAdapter.notifyDataSetChanged()
     }
 
 }
